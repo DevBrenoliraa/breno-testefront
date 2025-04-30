e893b4bf4bedbf8c40afefb1ef4c040d4aa8710d
Breno Lira Rocha <brenolirarocha5@gmail.com>
2025-04-30 11:23:47 -0300

    uky

--- index.js
+++ index.js
@@ -3,19 +3,13 @@ const path = require('path');
 const app = express();
 const port = 3001;
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-const meuPort = path.join(__dirname, 'public');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(meuPort, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-const style = path.join('public', __dirname, 'styles')
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(style, 'style.css'));
-  });
 
 app.listen(port, () => {
   console.log(`Portifólio rodando na porta ${port}`);
